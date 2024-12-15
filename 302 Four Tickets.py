537cfd22c82397df48a7c44ebe18a2938215b5d8
Parvathareddy Nagavarun Kumar <119427732+Nagavarunkumarparvathareddy@users.noreply.github.com>
2024-12-15 21:53:45 +0530

    Create 302 Four Tickets.py

---
+++ 302 Four Tickets.py
@@ -0,0 +1,10 @@
+# cook your dish here
+def movie():
+    x = int(input())
+    if x*4<= 1000:
+        print('yes')
+    else:
+        print('no')
+t = int(input())
+for i in range(t):
+    movie()
