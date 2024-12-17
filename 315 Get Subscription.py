ad6e6feb0fe55266a827be8718f7d5fd8b81df33
Parvathareddy Nagavarun Kumar <119427732+Nagavarunkumarparvathareddy@users.noreply.github.com>
2024-12-17 22:54:35 +0530

    Create 315 Get Subscription.py

---
+++ 315 Get Subscription.py
@@ -0,0 +1,10 @@
+# cook your dish here
+def premium():
+    x = int(input())
+    if x <= 30:
+        print('NO')
+    else:
+        print('yes')
+t = int(input())
+for i in range(t):
+    premium()
